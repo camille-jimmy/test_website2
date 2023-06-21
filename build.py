c672e30f0c5da6c340e4871c1d919a9f0c894699
Camille Chardin <camille.chardin@jimmy-energy.eu>
2023-06-21 12:59:54 +0200

    Commit avant mise en ligne site

--- build.py
+++ build.py
@@ -1,13 +1,12 @@
 import subprocess, sys, os
 from bs4 import BeautifulSoup
 import shutil, re
-from git import Repo
-
-os.chdir('test_mise_en_ligne_site')
-folder_path = os.getcwd()
 
 def main_build_doc():
+    subprocess.run(["git", "config", "--global", "core.autocrlf", "false"])
     folder_path=sys.argv[1]
+    gh_pages_branche_name = "gh-pages"
+    os.chdir(folder_path)
     try:
         shutil.rmtree(os.path.join(folder_path, "_build"))
     except:
@@ -18,27 +17,42 @@ def main_build_doc():
     add_password_page(folder_path)
     with open(os.path.join(folder_path, "_build", "html", ".nojekyll"), "w") as f:
         pass
-    
+    delete_branch(gh_pages_branche_name)
+    create_gh_pages_branch_from_site_dir(gh_pages_branche_name)
 
-def delete_branch(folder_path):
-    gh_pages_branche_name = "gh-pages"
-    repo = Repo(folder_path)
-    repo.config_writer().set_value("core", "autocrlf", "false").release()
-    if gh_pages_branche_name != repo.active_branch.name:
-        repo.index.add("*")
-        repo.index.commit("Commit avant mise en ligne site")
-        repo.git.push()
-    repo.git.checkout(gh_pages_branche_name)
-    repo.git.rm('-r', '*')
-    repo.git.push('origin', gh_pages_branche_name)
-    repo.delete_head(gh_pages_branche_name, force=True)
-    repo.git.push('origin', '--delete', gh_pages_branche_name)
+def git_checkout(command, block=True):
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT)
+        print("Commande exécutée avec succès :\n", output.decode())
+        
+    except subprocess.CalledProcessError as e:
+        print("Une erreur est survenue pendant l'exécution de la commande git checkout :")
+        if block:
+            raise ValueError("Message d'erreur :", e.output.decode())
+        else:
+            print("Message d'erreur :", e.output.decode())
 
-def create_gh_pages_branch_from_site_dir(folder_path):
-    gh_pages_branche_name = "gh-pages"
-    os.chdir(folder_path)
-    subprocess.run(['git', 'subtree', 'split', '--prefix', "_build/html", '--branch', gh_pages_branche_name])
-    subprocess.run(['git', 'push', 'origin', gh_pages_branche_name])
+
+def delete_branch(gh_pages_branche_name):
+    git_checkout(['git', 'add', '.'])
+    git_checkout(['git', 'commit', '-m', "Commit avant mise en ligne site"])
+    git_checkout(['git', 'push', 'origin', 'main', '--force'])
+    try:
+        git_checkout(["git", "checkout", gh_pages_branche_name])
+    except:
+        git_checkout(["git", "checkout", '-b', gh_pages_branche_name])
+    git_checkout(['git', 'add', '.'], False)
+    git_checkout(['git', 'commit', '-m', "Commit avant mise en ligne site"], False)
+    git_checkout(['git', 'push', 'origin', gh_pages_branche_name, '--force'])
+    git_checkout(["git", "rm", "-r", "*"], False)
+    git_checkout(["git", "push", "origin", gh_pages_branche_name, '--force'])
+    git_checkout(["git", "checkout", "main"])
+    git_checkout(["git", "branch", "-D", gh_pages_branche_name])
+    git_checkout(["git", "push", "origin", "--delete", gh_pages_branche_name])
+
+def create_gh_pages_branch_from_site_dir(gh_pages_branche_name):
+    git_checkout(['git', 'subtree', 'split', '--prefix', "_build/html", '--branch', gh_pages_branche_name])
+    git_checkout(['git', 'push', 'origin', gh_pages_branche_name])
 
 
 def add_jv_control(folder_path):
@@ -53,13 +67,14 @@ def add_jv_control(folder_path):
                 with open(file_path, 'r') as f:
                     contents = f.read()
                     soup = BeautifulSoup(contents, 'html.parser')
-                    p_tags = soup.find('body')
-                    p_tags.append("""<script>
+                    tag = soup.new_tag("script")
+                    tag.append("""
                             if (!localStorage.getItem('mdp')) {
                                 window.location.href = "../index.html";
                             }
-                        </script>
                         """)
+                    head = soup.find('body')
+                    head.insert_after(tag)
                 # Écrire le HTML modifié dans le fichier
                 with open(file_path, 'w') as f:
                     f.write(str(soup))
